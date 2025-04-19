a28b9a6ce63f1a82a58f4f62dfa81f3c5b7b0650
Suryaprakash <suryaprakashchary616@gmail.com>
2025-04-19 17:25:13 +0530

    tailwind working

--- my-react-app/src/Compoents/Navbar.jsx
+++ my-react-app/src/Compoents/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <>
-    <div className='bg-black'>hello world</div>
+    <div className='bg-black text-2xl ml-4 '>hello world</div>
     <div className="navbar">
       <Link to="/history"><button>History</button></Link>
       <Link to="/oxygen"><button>Oxygen Percentage</button></Link>
