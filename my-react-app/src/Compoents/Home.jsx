4c2cf2fcbbb3677547c2c74b2e6b32fe10e6cb77
pardhasaradhi1912 <pardhasaradhireddy2002@gmail.com>
2025-04-19 11:57:28 +0530

    removed hello

--- my-react-app/src/Compoents/Home.jsx
+++ my-react-app/src/Compoents/Home.jsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
       <p>Welcome to the Home page!</p>
       <Link to="/login">Go to Login</Link>
     </div>
